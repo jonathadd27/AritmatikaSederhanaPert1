7d8528677197ca899dc52a73a80efdab15aa11b8
jonathadd27 <90759969+jonathadd27@users.noreply.github.com>
2021-09-20 14:05:36 +0700

    Add files via upload

---
+++ AritmatikaSederhana.py
@@ -0,0 +1,34 @@
+#Program Hello World !!
+print("Hello World")
+print("Saya sedang belajar bahasa pemrograman Python")
+print("")
+print("")
+print("Berikut program latihan Aritmatika sederhana dengan inline variable Python")
+print("")
+print("")
+#Program Aritmatika Dasar
+bil1 = 25
+bil2 = 50
+penjumlahan = bil1 + bil2
+pengurangan = bil2 - bil1
+perkalian = bil1 * bil2
+pembagian = bil2 / bil1
+
+print("Diketahui")
+print("==============================")
+print("Bilangan Pertama =", bil1)
+print("Bilangan Kedua   =", bil2)
+print("")
+print("")
+
+print("Penghitungan :")
+print("==============================")
+print("Hasil Penjumlahan dari", bil1, "+", bil2, "adalah", penjumlahan)
+print("Hasil Pengurangan dari", bil2, "-", bil1, "adalah", pengurangan)
+print("Hasil Perkalian   dari", bil1, "*", bil2, "adalah", perkalian)
+print("Hasil Pembagian   dari", bil2, "/", bil1, "adalah", pembagian)
+
+print("")
+print("==============================")
+print("")
+
